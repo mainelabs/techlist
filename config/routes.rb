b17cdf587babd2afdca27e4b8bd6c61015830dd0
Sébastien Charrier <scharrier@gmail.Com>
2014-07-11 23:56:14 +0200

    Initialize admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :places, only: [:create, :index]
