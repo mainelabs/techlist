d72056a70bb68122db4af1f2e0e62eb70cc44391
Sébastien Charrier <scharrier@gmail.Com>
2014-07-11 23:43:12 +0200

    A user can list places

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :places, only: [:create]
+      resources :places, only: [:create, :index]
     end
   end
 end
