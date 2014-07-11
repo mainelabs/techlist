14382fffbfee985e335dc782b21d4a36466c0398
Sébastien Charrier <scharrier@gmail.Com>
2014-07-12 00:04:57 +0200

    User correct timezone and datetime format in json serialization

--- config/application.rb
+++ config/application.rb
@@ -28,13 +28,9 @@ module Techlist
       generate.view_specs false
     end
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # Handling time
+    config.time_zone = 'Europe/Paris'
+    config.active_support.time_precision = 0
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
