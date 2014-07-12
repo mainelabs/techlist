3f434ff5ef8e9c8619daf990b953a7f13aa7cb06
Sébastien Charrier <scharrier@gmail.Com>
2014-07-12 14:18:30 +0200

    Customize place admin form

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,22 @@ class Place < ActiveRecord::Base
 
   validates :name, presence: true
   validates :kind, presence: true, inclusion: { in: KINDS }
+
+  rails_admin do
+    edit do
+      field :name
+      field :kind
+      field :address
+      field :zip_code
+      field :city
+      field :url
+      field :description
+      field :owner_name
+      field :owner_email
+    end
+  end
+
+  def kind_enum
+    KINDS
+  end
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -26,9 +26,5 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
   end
 end
