484f1141e5a0641c666231c5f7df9bec241c2971
Sébastien Charrier <scharrier@gmail.Com>
2014-10-08 23:13:29 +0200

    Remove the create place api endpoint : needs more work to be useful

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,37 +1,9 @@
 class Api::V1::PlacesController < Api::V1::BaseController
-  def create
-    place = Place.new(place_params)
-    if place.save
-      render json: place
-    else
-      render json: {
-        message: 'Place is not valid',
-        errors: place.errors
-      }, status: :unprocessable_entity
-    end
-  end
-
   def index
     render json: Place.active
   end
 
   def show
     render json: Place.find(params[:id])
-  end
-
-  private
-
-  def place_params
-    params.permit(:name,
-                  :kind,
-                  :latitude,
-                  :longitude,
-                  :street,
-                  :zip_code,
-                  :city,
-                  :url,
-                  :description,
-                  :owner_name,
-                  :owner_email)
   end
 end
