a26248c07875635233927a14785817e043076ff4
Sébastien Charrier <scharrier@gmail.Com>
2014-10-09 02:21:13 +0200

    Display places updates in the admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -46,6 +46,38 @@ RailsAdmin.config do |config|
     end
   end
 
+  config.model PlaceUpdate do
+    list do
+      filters [:state, :kind]
+      field :state
+      field :kind
+      field :name
+      field :url
+      field :owner_email
+    end
+
+    edit do
+      field :name
+      field :kind, :enum do
+        enum do
+          Kind.codes
+        end
+      end
+      field :street
+      field :zip_code
+      field :city
+      field :country_code
+      field :latitude
+      field :longitude
+      field :url
+      field :twitter_name
+      field :logo_url
+      field :description
+      field :owner_name
+      field :owner_email
+    end
+  end
+
   config.model User do
     list do
       field :id
