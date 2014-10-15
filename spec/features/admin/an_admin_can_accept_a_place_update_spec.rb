d86bb6836f49cff4385c9d3031183b2cc6d25fa8
Sébastien Charrier <scharrier@gmail.Com>
2014-10-16 01:02:11 +0200

    Add specs for accept/reject place updates

---
+++ spec/features/admin/an_admin_can_accept_a_place_update_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'An admin accepts a place update' do
+  scenario "not pending place updates don't have the accept link" do
+    active = create(:place_update, :in_angers_with_coordinates, :active)
+    login_as(create(:admin))
+
+    visit rails_admin.edit_path(model_name: 'PlaceUpdate', id: active.id)
+
+    expect(page).to_not have_link(t('admin.actions.accept_update.menu'))
+  end
+
+  scenario 'pending place updates have the accept link' do
+    pending = create(:place_update, :in_angers_with_coordinates)
+    login_as(create(:admin))
+
+    visit rails_admin.edit_path(model_name: 'PlaceUpdate', id: pending.id)
+
+    expect(page).to have_link(t('admin.actions.accept_update.menu'))
+  end
+
+  scenario 'accepts the update' do
+    pending = create(:place_update, :in_angers_with_coordinates)
+    login_as(create(:admin))
+
+    visit rails_admin.edit_path(model_name: 'PlaceUpdate', id: pending.id)
+    click_link(t('admin.actions.accept_update.menu'))
+
+    pending.reload
+    expect(pending).to be_active
+  end
+end
