4adeff04c7965660eba969494fbb22ebddbdd43b
Sébastien Charrier <scharrier@gmail.Com>
2014-10-23 23:49:28 +0200

    Protect the staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,7 +3,11 @@ require_relative 'production'
 Mail.register_interceptor RecipientInterceptor.new(ENV['EMAIL_RECIPIENTS'])
 
 Rails.application.configure do
-  # ...
+  if ENV['STAGING_USERNAME']
+    config.middleware.insert_after(::Rack::Runtime, "::Rack::Auth::Basic", "Staging") do |u, p|
+      [u, p] == [ENV['STAGING_USERNAME'], ENV['STAGING_PASSWORD']]
+    end
+  end
 
   config.action_mailer.default_url_options = { host: 'staging.techlist.com' }
 
