c9d7c825b00faf46e8dcb42f0f6125f655a1f07d
Sébastien Charrier <scharrier@gmail.Com>
2015-05-14 22:38:40 +0200

    Add a 'Tous les lieux' link to navigation

--- spec/features/user_lists_places_spec.rb
+++ spec/features/user_lists_places_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 feature 'A user lists places' do
+  scenario 'by clicking on the navigation link' do
+    create_list(:place, 3, :active)
+
+    visit root_path
+    click_link 'Tous les lieux'
+
+    expect(page).to have_content('3 lieux')
+  end
+
   scenario 'and views all places details' do
     place = create(:place, :active)
 
@@ -9,14 +18,6 @@ feature 'A user lists places' do
     expect(page).to have_content(place.name)
     expect(page).to have_content(place.url)
     expect(page).to have_content(t("kinds.codes.#{place.kind}"))
-  end
-
-  scenario 'and views the total count' do
-    create_list(:place, 7, :active)
-
-    visit places_path
-
-    expect(page).to have_content('7 lieux')
   end
 
   scenario 'and navigates between pages' do
