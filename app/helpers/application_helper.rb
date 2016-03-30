a23f07c0fd669d00b3defca0822fd4ba69b46092
Sébastien Charrier <sebastien.charrier@capitainetrain.com>
2016-03-30 22:24:20 +0200

    Enhance meta tags with places attributes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,4 @@ module ApplicationHelper
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
     @markdown.render(markdown_text).html_safe if markdown_text
   end
-
-  def title(page_title)
-    content_for(:title, page_title)
-  end
 end
