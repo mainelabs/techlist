141dafa3e297285ec3fc7d6731c8e91e9d26372c
Sébastien Charrier <sebastien.charrier@capitainetrain.com>
2016-03-31 23:26:00 +0200

    Use turbolinks to submit searches

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,15 @@
 //= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree ./components
 //= require_self
 
-$(document).on('ready page:load', function() {
+$(document).on('turbolinks:load', function() {
+  // Use turbolinks to submit search form
+  $('.search-form').on('submit', function(event) {
+    Turbolinks.visit(this.action + '?' + $(this).serialize());
+    event.preventDefault();
+  });
+
   $('.place-map').mapWithMarker();
 });
