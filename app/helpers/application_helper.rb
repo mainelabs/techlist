5e8c555a216e4225f55d743ed81e1c313409695c
Sébastien Charrier <sebastien.charrier@capitainetrain.com>
2016-04-21 22:07:15 +0200

    Add a link to Google maps for a given place

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
     @markdown.render(markdown_text).html_safe if markdown_text
   end
+
+  def map_link_to(place)
+    link_to place.human_address, "http://www.google.com/maps/place/#{place.latitude},#{place.longitude}", target: '_blank'
+  end
 end
