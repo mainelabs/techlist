1e4c641d7f69dbe4d79e0002680d96817ea242f3
Sébastien Charrier <sebastien.charrier@capitainetrain.com>
2016-06-18 23:27:33 +0200

    Display login form correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  def body_class(options = {})
+    qualified_controller_name = controller.controller_path.gsub('/','-')
+    body_class = "page-#{qualified_controller_name} page-#{qualified_controller_name}-#{controller.action_name}"
+
+    body_class
+  end
+
   def markdown(markdown_text)
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
     @markdown.render(markdown_text).html_safe if markdown_text
