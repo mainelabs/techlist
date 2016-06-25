048912c54ed7d7e25e43a420e997b7c3625c4113
Sébastien Charrier <sebastien.charrier@capitainetrain.com>
2016-06-25 23:01:18 +0200

    Merge redundant specs files

--- spec/features/user_edits_a_place_spec.rb
+++ spec/features/user_edits_a_place_spec.rb
@@ -35,4 +35,35 @@ feature 'A user edits a place' do
 
     expect(current_path).to eq(place_path(place))
   end
+
+  scenario 'and views fields already filled with original values' do
+    place = create(:place, :active)
+
+    visit edit_place_path(place)
+
+    expect(find_field('Nom').value).to eq(place.name)
+    expect(page).to have_css("meta[name='robots'][content='noindex']", visible: false)
+  end
+
+  scenario 'and fills all required fields' do
+    place = create(:place, :active)
+
+    visit edit_place_path(place)
+    fill_in 'Nom', with: 'Updated place name'
+    fill_in 'Votre nom', with: place.owner_name
+    fill_in 'Votre adresse email', with: place.owner_email
+    click_button 'Mettre à jour le lieu'
+
+    place_update = PlaceUpdate.last
+    expect(page).to have_content('a été correctement sauvegardée')
+    expect(place_update.place).to eq(place)
+    expect(place_update.name).to eq('Updated place name')
+    expect(place_update.state).to eq('pending')
+  end
+
+  scenario 'but not pending ones' do
+    place = create(:place)
+
+    expect { visit edit_place_path(place) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
