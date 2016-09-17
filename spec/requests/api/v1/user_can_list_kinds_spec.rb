b3a4bef4f8d7e0e38213bd3188096921926e9549
Sébastien Charrier <sebastien.charrier@capitainetrain.com>
2016-09-17 16:24:12 +0200

    Remove deprecation warnings

--- spec/requests/api/v1/user_can_list_kinds_spec.rb
+++ spec/requests/api/v1/user_can_list_kinds_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'GET /api/v1/kinds' do
   it 'returns kinds list' do
-    get '/api/v1/kinds', nil, headers
+    get '/api/v1/kinds', headers: headers
 
     expect(response_json['kinds'].length).to eq Kind.codes.length
   end
--- spec/requests/api/v1/user_can_list_places_spec.rb
+++ spec/requests/api/v1/user_can_list_places_spec.rb
@@ -5,7 +5,7 @@ describe 'GET /api/v1/places' do
     create_list(:place, 5, :active)
     create(:place)
 
-    get '/api/v1/places', nil, headers
+    get '/api/v1/places', headers: headers
 
     expect(response_json['places'].length).to eq 5
   end
