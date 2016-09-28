86e15fb1431b6ec6a1ff8de233c4ebae3343d226
Sébastien Charrier <sebastien.charrier@capitainetrain.com>
2016-09-28 23:39:32 +0200

    Return a Search default results

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,17 @@
 class Search < ApplicationRecord
+  extend FriendlyId
+
   validates :page_title, presence: true
   validates :results_one, presence: true
   validates :results_other, presence: true
   validates :slug, presence: true
+
+  friendly_id :slug, use: :slugged
+
+  def results
+    results = Place
+    results.where(kind: kind) if kind
+    results.q(q) if q
+    results
+  end
 end
